0a6a38752de54711823afb5b832dc3d6b980f675
James Clarke <james.clarke@tcu.edu>
2024-08-06 14:47:38 -0500

    none-ls

--- init.lua
+++ init.lua
@@ -300,6 +300,19 @@ require("lazy").setup({
                 }
             end
         },
+        {
+            "vimtools/none-ls.nvim",
+            config = function()
+                local null_ls = require("null-ls")
+                null_ls.setup({
+                    sources = {
+                        null_ls.builtins.formatting.stylua,
+                        null_ls.builtins.completion.spell,
+                        require("none-ls.diagnostics.eslint"),
+                    },
+                })
+            end
+        },
     },
     -- Automatically check for plugin updates
     checker = { enabled = true },
